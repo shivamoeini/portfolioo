ee9692fad37b70dce81c859d65a5873df2421e2e
shiva moeini <87569940+shivamoeini@users.noreply.github.com>
2023-03-16 00:14:25 +0330

    footer-mailchip

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,29 @@
-import {Container} from "react-bootstrap"
-import {Row, Col } from "react-bootstrap";
-import { MilchimpForm } from "./MailchimpForm";
-import logo from '../assets/img/logo.svg';
-import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
-export const Footer=()=>{
-return(
-    <Footer>
-        <Container>
-            <Row className="align-item-center">
-                <MilchimpForm/>
-                <Col  sm={6}>
-                    <img src={logo} alt="Logo"/>
-                </Col>
-                <Col sm={6} className="text-center text-sm-end">
-                    <div className="social-icon">
-                        <a href=""><img src={navIcon1}/></a>
-                        <a href=""><img src={navIcon2}/></a>
-                        <a href=""><img src={navIcon3}/></a>
-                     </div>
-                     <p>CopyRight 2022. All Right Reserved</p>
-                </Col>
-            </Row>
-        </Container>
-    </Footer>
-)
+import { Container, Row, Col } from "react-bootstrap";
+import { MailchimpForm } from "./MailchimpForm";
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <MailchimpForm />
+          <Col size={12} sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col size={12} sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href="#"><img src={navIcon1} alt="Icon" /></a>
+              <a href="#"><img src={navIcon2} alt="Icon" /></a>
+              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+            </div>
+            <p>Copyright 2022. All Rights Reserved</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
 }
--- src/components/MailchimpForm.jsx
+++ src/components/MailchimpForm.jsx
@@ -1,21 +1,21 @@
 import MailchimpSubscribe from "react-mailchimp-subscribe";
-export const MilchimpForm=()=>{
-    const postUrl=`${process.env.REACT_APP_MAILCHIMP_URL}?U=${process.env.REACT_APP_MAILCHIMP_U}$id=${process.env.REACT_APP_MAILCHIMP_ID}`
+import { Newsletter } from "./Newsletter";
 
-    return(
-        <>
-        <MailchimpSubscribe
+export const MailchimpForm = () => {
+  const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
+
+  return (
+    <>
+      <MailchimpSubscribe
         url={postUrl}
-        render={({subscribe,status,message})=>(
-            <Newsletter 
-            
+        render={({ subscribe, status, message }) => (
+          <Newsletter
             status={status}
             message={message}
-            onValidated={formData=>subscribe(formData)}
-            
+            onValidated={formData => subscribe(formData)}
             />
         )}
         />
-        </>
-    )
+    </>
+  )
 }
